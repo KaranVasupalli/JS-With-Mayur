7500fa2571fd52dea7cba2e50ea7b5b0e9c23bca
Karan Vasupalli <vkaran14210@gmail.com>
2024-07-08 11:23:56 +0530

    add

---
+++ index.js
@@ -0,0 +1,138 @@
+// let arr = [1,2,3,"hello",true]
+// arr=[5,6,7]
+// console.log(arr)
+
+
+// var arr1 = [1,2,3,"hello",true]
+
+// arr1=[1,4,3,0]
+// console.log(arr1)
+
+
+// let x = 6;
+// {
+//     console.log(x)
+// }
+
+// {
+//     var y = 8;
+// }
+// console.log(y);
+
+
+
+//Data Types
+
+//strings
+
+// let firstName = "Mayur"
+// let lastName = "Yelmule"
+
+// console.log(firstName + ' ' + lastName);
+
+// let num1 = 6;
+// let name1 = "mercedes" 
+
+// console.log(typeof(num1 + ' ' + name1));
+
+
+
+//number
+
+// let x = 15
+// let y = 72
+
+// console.log(x+y);
+
+// let mayur = false;
+// if(mayur){
+//     console.log("i l u");
+// }
+// else{
+//     console.log("i h u");
+// }
+
+
+//undefined
+// let vinay;
+
+// console.log(vinay);
+
+
+// let vinay=null;
+
+// console.log(vinay);
+
+
+
+//object
+// let username = {
+//     firstName: "ashray",
+//     lastName: "yenpreddiwar",
+//     email: "@gmail.com",
+//     age: 22
+// }
+
+// console.log(typeof(username.age))
+
+
+
+//Array
+
+
+
+// for(let i =0 ; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+// let arr = [1,3,5,5,7]
+// arr.map((arr1)=>{
+//     console.log(arr1);
+// })
+
+
+
+// function
+
+
+
+// function func(){
+//     let username = {
+//             firstName: "ashray",
+//             lastName: "yenpreddiwar",
+//             email: "@gmail.com",
+//             age: 22
+//         }
+//     console.log(username);
+// }
+// func()
+
+// const func1 = ()=> {
+//     let username = {
+//         firstName: "ashray",
+//         lastName: "yenpreddiwar",
+//         email: "@gmail.com",
+//         age: 22
+//     }
+//     console.log(username);
+// }
+
+// func1()
+
+
+// let age = 19;
+
+// if(age > 18){
+//     console.log("i can vote");
+// }
+// else{
+//     console.log("i can't vote");
+// }
+
+// let vote = age >= 18 ? "i can vote" : "i can't vote"
+
+// console.log(age);
+// console.log(vote);s
+
+
+let name1 = prompt("please enter ")
+console.log(name1);
